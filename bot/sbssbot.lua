77da4a440e70115456b5cb898e7608ea3a7763a7
godzilaaa <sampayervand@gmail.com>
2016-03-27 18:02:30 +0430

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -220,28 +220,20 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {122774063,106014613,108151290,114986931,121208333},--Sudo users
+    sudo_users = {173273482,186106896,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
-    realm = {79472297},--Realms Id
     moderation = {data = 'data/moderation.json'},
-    about_text = [[SBSS Version 2
-An advance Administration bot based on yagop/telegram-bot by @Mr_Ah_S
+    about_text = [[alpha Version 2
 
-https://Github.com/MRAHS/SBSS
 
 Admins
-@MR_AH_S [Developer]
+@soltoonalpha [Developer]
 @Ra3oul13 [Manager]
 @Amiirdada [Support]
-Other Admins:
-@Taha_sh_report
-@Sajjad0S
-@i81therock
 
-برای خرید گپ به 4 آیدی اول و برای خرید آموزش ساخت ربات ضد اسپم و ساخت ربات ضد اسپم به @Mr_Ah_S مراجعه کنید
+جهت خرید گپ ضد اسپم به ایدی زیر مراجعه کنید
+"soltoonalpha
 
-Our channels
-@SBSS_Team
 ]],
     help_text = [[
 لیست دستورات 
